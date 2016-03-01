de2b4980ee94e83ec7f6f9e42f7dc8f5f73c7022
酌晨茗 <boleketang@ruoyimingdeiMac.local>
2016-03-01 18:06:15 +0800

    add
    增加别的路径的plist文件的增删改查

--- PlistManager.h
+++ PlistManager.h
@@ -8,13 +8,44 @@
 
 #import <Foundation/Foundation.h>
 
+#define PlistName @"info"
+
 #define Name @"name"
 #define PassWord @"password"
 
 @interface PlistManager : NSObject
 
-//创建
-+ (void)createPlistWithName:(NSString *)name;
+//fileName不需要后缀名
++ (NSString *)documentDicWithFileName:(NSString *)fileName;
+
++ (NSString *)libraryDicWithFileName:(NSString *)fileName;
+
+//会自动创建文件夹
++ (NSString *)documentDicWithDic:(NSString *)dicName fileName:(NSString *)fileName;
+
++ (NSString *)libraryDicWithDic:(NSString *)dicName fileName:(NSString *)fileName;
+
+#pragma mark - 操作自己创建的plist文件
+//创建 path是文件路径 要添加plist文件名和后缀
++ (void)createPlistWithPath:(NSString *)path;
+
+//写入其他数据
++ (void)writeToPlistWithKey:(NSString *)key
+                      value:(id)value
+                       path:(NSString *)path;
+
++ (void)writeToPlistWithDic:(NSDictionary *)dic
+                       path:(NSString *)path;
+
+//读取
++ (id)readDataWithKey:(NSString *)key
+                 path:(NSString *)path;
+
++ (id)readAllDataWithPath:(NSString *)path;
+
+
+#pragma mark - 只能操作默认路径的plist文件
++ (void)createPlist;
 
 + (void)name:(NSString *)name
     password:(NSString *)password;
@@ -23,7 +54,12 @@
 + (void)writeToPlistWithKey:(NSString *)key
                       value:(id)value;
 
++ (void)writeToPlistWithDic:(NSDictionary *)key;
+
 //读取
 + (id)readDataWithKey:(NSString *)key;
 
++ (id)readAllData;
+
 @end
+
--- PlistManager.m
+++ PlistManager.m
@@ -7,32 +7,56 @@
 //
 
 #import "PlistManager.h"
-#import <objc/runtime.h>
-
-static char PathKey;
-
-#define CustomName @"info"
 
 @implementation PlistManager
 
-+ (void)createPlistWithName:(NSString *)name {
-    if (name == nil) {
-        name = CustomName;
-    }
-    
++ (NSString *)documentDicWithFileName:(NSString *)fileName {
     NSString *documentPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
-    NSString *plistPath = [documentPath stringByAppendingString:[NSString stringWithFormat:@"/%@.plist", name]];
-    
-    if (![[NSFileManager defaultManager] fileExistsAtPath:plistPath]) {
-        [[NSFileManager defaultManager] createFileAtPath:plistPath contents:nil attributes:nil];
-    }
-    
-    objc_setAssociatedObject(self, &PathKey, plistPath, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    NSString *plistPath = [documentPath stringByAppendingString:[NSString stringWithFormat:@"/%@.plist", fileName]];
+    return plistPath;
 }
 
++ (NSString *)libraryDicWithFileName:(NSString *)fileName {
+    NSString *libraryPath = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES).firstObject;
+    NSString *plistPath = [libraryPath stringByAppendingString:[NSString stringWithFormat:@"/%@.plist", fileName]];
+    return plistPath;
+}
+
++ (NSString *)documentDicWithDic:(NSString *)dicName fileName:(NSString *)fileName {
+    NSString *documentPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+    NSString *plistPath = [documentPath stringByAppendingString:[NSString stringWithFormat:@"/%@/%@.plist", dicName, fileName]];
+    
+    NSString *dicPath = [documentPath stringByAppendingString:[NSString stringWithFormat:@"/%@/", dicName]];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:dicName]) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:dicPath withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    
+    return plistPath;
+}
+
++ (NSString *)libraryDicWithDic:(NSString *)dicName fileName:(NSString *)fileName {
+    NSString *libraryPath = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES).firstObject;
+    NSString *plistPath = [libraryPath stringByAppendingString:[NSString stringWithFormat:@"/%@/%@.plist", dicName, fileName]];
+    
+    NSString *dicPath = [libraryPath stringByAppendingString:[NSString stringWithFormat:@"/%@/", dicName]];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:dicName]) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:dicPath withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    
+    return plistPath;
+}
+
+#pragma mark - 操作自己创建的plist文件
++ (void)createPlistWithPath:(NSString *)path {
+    if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        [[NSFileManager defaultManager] createFileAtPath:path contents:nil attributes:nil];
+    }
+}
+
+//写入其他数据
 + (void)writeToPlistWithKey:(NSString *)key
-                      value:(id)value {
-    NSString *path = objc_getAssociatedObject(self, &PathKey);
+                      value:(id)value
+                       path:(NSString *)path {
     NSMutableDictionary *dic = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
     if (dic == nil) {
         dic = [NSMutableDictionary dictionary];
@@ -41,23 +65,25 @@ static char PathKey;
     [dic writeToFile:path atomically:YES ];
 }
 
-+ (void)name:(NSString *)name
-    password:(NSString *)password {
-    NSString *path = objc_getAssociatedObject(self, &PathKey);
-    NSMutableDictionary *dic = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
-    if (dic == nil) {
-        dic = [NSMutableDictionary dictionary];
++ (void)writeToPlistWithDic:(NSDictionary *)dic
+                       path:(NSString *)path {
+    NSMutableDictionary *mDic = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+    if (mDic == nil) {
+        mDic = [NSMutableDictionary dictionary];
     }
- 
-    [dic setObject:name forKey:Name];
-    [dic setObject:password forKey:PassWord];
-
-    [dic writeToFile:path atomically:YES ];
+    //    NSArray *allKey = [dic allKeys];
+    //    for (int i = 0; i < allKey.count; i++) {
+    //        [mDic setObject:[dic objectForKey:allKey[i]] forKey:allKey[i]];
+    //    }
+    
+    [mDic addEntriesFromDictionary:dic];
+    
+    [dic writeToFile:path atomically:YES];
 }
 
 //读取
-+ (id)readDataWithKey:(NSString *)key {
-    NSString *path = objc_getAssociatedObject(self, &PathKey);
++ (id)readDataWithKey:(NSString *)key
+                 path:(NSString *)path {
     NSData *plistData = [NSData dataWithContentsOfFile:path];
     
     NSString *plistError;
@@ -67,4 +93,63 @@ static char PathKey;
     return [(NSArray *)plist valueForKey:key];
 }
 
++ (id)readAllDataWithPath:(NSString *)path {
+    NSData *plistData = [NSData dataWithContentsOfFile:path];
+    
+    NSString *plistError;
+    NSPropertyListFormat format;
+    
+    id plist = [NSPropertyListSerialization propertyListFromData:plistData mutabilityOption:NSPropertyListImmutable format:&format errorDescription:&plistError];
+    return (NSDictionary *)plist;
+}
+
+#pragma mark - 操作默认的plist文件
++ (NSString *)filePath {
+    NSString *documentPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+    NSString *plistPath = [documentPath stringByAppendingString:[NSString stringWithFormat:@"/%@.plist", PlistName]];
+    return plistPath;
+}
+
++ (void)createPlist {
+    NSString *plistPath = [self filePath];
+    [self createPlistWithPath:plistPath];
+}
+
++ (void)writeToPlistWithKey:(NSString *)key
+                      value:(id)value {
+    NSString *path = [self filePath];
+    [self writeToPlistWithKey:key value:value path:path];
+}
+
++ (void)writeToPlistWithDic:(NSDictionary *)dic {
+    NSString *path = [self filePath];
+    [self writeToPlistWithDic:dic path:path];
+}
+
++ (void)name:(NSString *)name
+    password:(NSString *)password {
+    NSString *path = [self filePath];
+    NSMutableDictionary *dic = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+    if (dic == nil) {
+        dic = [NSMutableDictionary dictionary];
+    }
+    
+    [dic setObject:name forKey:Name];
+    [dic setObject:password forKey:PassWord];
+    
+    [dic writeToFile:path atomically:YES];
+}
+
+//读取
++ (id)readDataWithKey:(NSString *)key {
+    NSString *path = [self filePath];
+    return [self readDataWithKey:key path:path];
+}
+
++ (id)readAllData {
+    NSString *path = [self filePath];
+    return [self readAllDataWithPath:path];
+}
+
 @end
+
